38ee02886b02ef7624398c1da0324bce1729bb91
vasikoukhurgunashvili <vasiko.ukhurgunashvili.1@btu.edu.ge>
2026-01-20 20:58:21 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -250,3 +250,19 @@ function getRandomComment() {
   ];
   return comments[Math.floor(Math.random() * comments.length)];
 }
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollTopBtn.classList.add("show");
+  } else {
+    scrollTopBtn.classList.remove("show");
+  }
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
